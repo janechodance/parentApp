ef188ad9eace89e5ad5fb8e43caa5aa80773c461
Jane Cho <janechodance@gmail.com>
2023-12-08 23:59:50 -0500

    summary page

--- app/completeSummary.tsx
+++ app/completeSummary.tsx
@@ -1,4 +1,8 @@
-import { View, Text, ScrollView, StyleSheet, Image } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import Confetti from "../assets/background/confetti.svg";
+import ConfettiBottom from "../assets/background/confettiBottom.svg";
+import OneButtonFooter from "./component/footer/oneButtonFooter";
+import ActivityHighlight from "./component/activityHighlight";
 
 export default function CompleteSummary() {
   return (
@@ -6,7 +10,23 @@ export default function CompleteSummary() {
       style={styles.background}
       contentContainerStyle={{ alignItems: "center" }}
     >
-      <Text>Summary</Text>
+      <View style={styles.container}>
+        <View>
+          <Confetti style={{ position: "absolute" }} />
+          <Text style={styles.headerText}>Awesome Job!</Text>
+          <Text style={styles.headerContentText}>
+            Thank you for tracking your progress! This information helps us make
+            better recommendations for you and your family.
+          </Text>
+        </View>
+        <View style={styles.highlightContainer}>
+          <ConfettiBottom style={{ position: "absolute" }} />
+          <ActivityHighlight />
+        </View>
+        <View>
+          <OneButtonFooter buttonText="View Progress Log" buttonTo="/" />
+        </View>
+      </View>
     </ScrollView>
   );
 }
@@ -16,8 +36,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFCFC",
   },
   container: {
-    backgroundColor: "black",
-    width: 100,
-    height: 100,
+    flex: 1,
+    width: 343,
+    marginTop: 24,
+  },
+  headerText: {
+    fontFamily: "Jost-Medium",
+    fontSize: 28,
+    lineHeight: 42,
+    marginTop: 64,
+  },
+  headerContentText: {
+    fontFamily: "Arimo-Regular",
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 24,
+  },
+  highlightContainer: {
+    marginTop: 56,
   },
 });
---
+++ app/component/activityHighlight.tsx
@@ -0,0 +1,8 @@
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+export default function ActivityHighlight() {
+  return (
+    <View>
+      <Text>Activity Hightlight</Text>
+    </View>
+  );
+}
